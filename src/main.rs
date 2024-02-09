7b1c2fed9ec209d0fb78016075a1ab6f6a616df3
K <Redknclarke@gmail.com>
2024-02-08 22:52:08 -0500

    switched from a struct to a single float for memory optimization

--- src/main.rs
+++ src/main.rs
@@ -12,86 +12,60 @@ use piston::window::WindowSettings;
 
 const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
-const SQUARE_SIZE: i32 = 10;
+const SQUARE_SIZE: i32 = 5;
 const ROWS: u32 = WIDTH / SQUARE_SIZE as u32;
 const COLS: u32 = HEIGHT / SQUARE_SIZE as u32;
 
-const BLACK: Color = Color {
-    h: 0.0,
-    v: 0.0,
-    s: 0.0,
-};
-#[derive(Clone, Copy)]
-struct Color {
-    h: f32,
-    s: f32,
-    v: f32,
-}
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-impl PartialEq for Color {
-    fn eq(&self, other: &Self) -> bool {
-        return self.as_rgba() == other.as_rgba();
-    }
-}
+fn to_rgba(h: f32) -> [f32; 4] {
+    let (h, s, v) = (h, 1.0, 1.0);
+    let c = v * s;
+    let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
+    let m = v - c;
 
-impl Color {
-    fn new(hue: f32, saturation: f32, value: f32) -> Color {
-        return Color {
-            h: hue,
-            s: saturation,
-            v: value,
-        };
-    }
+    let (r, g, b) = if h >= 0.0 && h < 60.0 {
+        (c, x, 0.0)
+    } else if h >= 60.0 && h < 120.0 {
+        (x, c, 0.0)
+    } else if h >= 120.0 && h < 180.0 {
+        (0.0, c, x)
+    } else if h >= 180.0 && h < 240.0 {
+        (0.0, x, c)
+    } else if h >= 240.0 && h < 300.0 {
+        (x, 0.0, c)
+    } else {
+        (c, 0.0, x)
+    };
 
-    fn as_rgba(&self) -> [f32; 4] {
-        let (h, s, v) = (self.h, self.s, self.v);
-        let c = v * s;
-        let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
-        let m = v - c;
-
-        let (r, g, b) = if h >= 0.0 && h < 60.0 {
-            (c, x, 0.0)
-        } else if h >= 60.0 && h < 120.0 {
-            (x, c, 0.0)
-        } else if h >= 120.0 && h < 180.0 {
-            (0.0, c, x)
-        } else if h >= 180.0 && h < 240.0 {
-            (0.0, x, c)
-        } else if h >= 240.0 && h < 300.0 {
-            (x, 0.0, c)
-        } else {
-            (c, 0.0, x)
-        };
-
-        [(r + m), (g + m), (b + m), 1.0]
-    }
+    [(r + m), (g + m), (b + m), 1.0]
 }
 
 struct Game {
     gl: GlGraphics,
-    arr: [[Color; COLS as usize]; ROWS as usize],
+    grid: [[f32; COLS as usize]; ROWS as usize],
     dragging: bool,
-    color: Color,
+    hue: f32,
 }
 
 impl Game {
     fn new(g: GlGraphics) -> Game {
         Game {
             dragging: false,
-            arr: [[BLACK; COLS as usize]; ROWS as usize],
+            grid: [[0.0; COLS as usize]; ROWS as usize],
             gl: g,
-            color: Color::new(0.0, 1.0, 1.0),
+            hue: 0.01,
         }
     }
 
     fn render(&mut self, arg: &RenderArgs) {
         self.gl.draw(arg.viewport(), |_c, gl| {
-            graphics::clear(BLACK.as_rgba(), gl);
+            graphics::clear(BLACK, gl);
             for i in 0..ROWS as usize {
                 for j in 0..COLS as usize {
-                    if self.arr[i][j] != BLACK {
+                    if self.grid[i][j] != 0.0 {
                         graphics::rectangle(
-                            self.arr[i][j].as_rgba(),
+                            to_rgba(self.grid[i][j]),
                             graphics::rectangle::square(
                                 (i * SQUARE_SIZE as usize) as f64,
                                 (j * SQUARE_SIZE as usize) as f64,
@@ -107,7 +81,7 @@ impl Game {
     }
 
     fn update(&mut self) {
-        let mut next_arr = self.arr;
+        let mut next_arr = self.grid;
 
         for x in 0..ROWS as usize {
             for y in 0..COLS as usize {
@@ -117,29 +91,29 @@ impl Game {
                 }
 
                 // Drop down
-                if self.arr[x][y] != BLACK && self.arr[x][y - 1] == BLACK {
-                    next_arr[x][y - 1] = self.arr[x][y];
-                    next_arr[x][y] = BLACK;
+                if self.grid[x][y] != 0.0 && self.grid[x][y - 1] == 0.0 {
+                    next_arr[x][y - 1] = self.grid[x][y];
+                    next_arr[x][y] = 0.0;
                 }
                 // Go left
                 else if x as i16 - 1 >= 0
-                    && self.arr[x][y] != BLACK
-                    && self.arr[x - 1][y - 1] == BLACK
+                    && self.grid[x][y] != 0.0
+                    && self.grid[x - 1][y - 1] == 0.0
                 {
-                    next_arr[x - 1][y - 1] = self.arr[x][y];
-                    next_arr[x][y] = BLACK;
+                    next_arr[x - 1][y - 1] = self.grid[x][y];
+                    next_arr[x][y] = 0.0;
                 }
                 // Go right
-                else if x as i16 + 1 < COLS as i16
-                    && self.arr[x][y] != BLACK
-                    && self.arr[x + 1][y - 1] == BLACK
+                else if x as i16 + 1 <= COLS as i16
+                    && self.grid[x][y] != 0.0
+                    && self.grid[x + 1][y - 1] == 0.0
                 {
-                    next_arr[x + 1][y - 1] = self.arr[x][y];
-                    next_arr[x][y] = BLACK;
+                    next_arr[x + 1][y - 1] = self.grid[x][y];
+                    next_arr[x][y] = 0.0;
                 }
             }
         }
-        self.arr = next_arr;
+        self.grid = next_arr;
     }
 
     fn process_mouse(&mut self, arg: [f64; 2]) {
@@ -149,16 +123,16 @@ impl Game {
         let x = arg[0] as i32 / SQUARE_SIZE;
         let y = arg[1] as i32 / SQUARE_SIZE;
         if x < ROWS as i32 && y < COLS as i32 {
-            self.arr[x as usize][y as usize] = self.color;
+            self.grid[x as usize][y as usize] = self.hue;
         }
-        self.color.h = (self.color.h + 0.075) % 360.0;
+        self.hue = (self.hue + 0.075) % 360.0 + 0.01;
     }
 
     fn process_input(&mut self, arg: &ButtonArgs) {
         match arg.state {
             ButtonState::Press => match arg.button {
                 Button::Mouse(MouseButton::Left) => self.dragging = true,
-                Button::Keyboard(Key::R) => self.arr = [[BLACK; COLS as usize]; ROWS as usize],
+                Button::Keyboard(Key::R) => self.grid = [[0.0; COLS as usize]; ROWS as usize],
                 _ => (),
             },
 
